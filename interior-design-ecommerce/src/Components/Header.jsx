aea20a67cc46e7542ce391250593f6f8921fca8c
Deborah-Teta <154221965+Deborah-Teta@users.noreply.github.com>
2025-10-19 23:30:37 +0200

    Updates on the header
    Made some changes in the styles

--- interior-design-ecommerce/src/Components/Header.jsx
+++ interior-design-ecommerce/src/Components/Header.jsx
@@ -9,7 +9,7 @@ function Header() {
             <h1 class="font-bold text-2xl shadow-md text-amber-800">Feel the Beauty</h1>
             <nav className='flex space-x-6'>
 
-                <Link to="/Home" className='text-green-200 hover:text-green-600 transition-colors font-bold'>Home</Link>
+                <Link to="/Home" className=' text-green-500 hover:text-green-900 transition-colors font-bold'>Home</Link>
                 <Link to="/About" className='text-green-500 hover:text-green-900 transition-colors font-bold'>About</Link>
                 <Link to="/Product" className='text-green-500 hover:text-green-900 transition-colors font-bold'>Product</Link>
                 <Link to="/Contact" className='text-green-500 hover:text-green-900 transition-colors font-bold'>Contact</Link>
